fcb0c6369e8353303abd7d270620159d5d7bc5f0
Pavel <Pasha_sss@tut.by>
2019-03-02 21:28:53 +0300

    adaptive

--- script/main.js
+++ script/main.js
@@ -1,18 +1,45 @@
-function showSlide(slideNumber) {
-    const slides = document.querySelector('.slider__inner').children;
-    let currentSlideIndex;
-    Array.prototype.forEach.call(slides, (slide, i) => {
-        if (!slide.classList.contains('hide')) {
-            slide.classList.add('hide');
-            currentSlideIndex = i;
-        }
-    });
-    let nextSlideIndex;
-    if (currentSlideIndex >= slides.length-1) {
-        nextSlideIndex = 0;
-    } else {
-        nextSlideIndex = currentSlideIndex + 1;
-    }
-    slides[nextSlideIndex].classList.remove('hide');
-}
-setInterval(showSlide, 2000);
+// function showSlide(slideNumber) {
+//     const slides = document.querySelector('.slider__inner').children;
+//     let currentSlideIndex;
+//     Array.prototype.forEach.call(slides, (slide, i) => {
+//         if (!slide.classList.contains('hide')) {
+//             slide.classList.add('hide');
+//             currentSlideIndex = i;
+//         }
+//     });
+//     let nextSlideIndex;
+//     if (currentSlideIndex >= slides.length-1) {
+//         nextSlideIndex = 0;
+//     } else {
+//         nextSlideIndex = currentSlideIndex + 1;
+//     }
+//     slides[nextSlideIndex].classList.remove('hide');
+// }
+// setInterval(showSlide, 2000);
+
+        // JQuery
+
+$(function() {
+
+    $('header .top nav div').on('click', function() {
+
+        $('header .top nav ul').slideToggle(300, function() {
+            if($(this).css('display') === 'none') {
+                $(this).removeAttr('style');
+            }
+        })
+    })
+
+    $(document).ready(function() {
+        $('.slider__inner').cycle({
+        fx: 'fade',
+        timeout: 1000 });
+        });
+
+                //height for images
+            
+    // var width = parseInt($('.about img').css('width'));
+    // var height = width / 3;
+    // $('.about img').css('height', height);
+
+})

d9960a31d0afd077896a0545bbe765a76d00d387
Pavel <Pasha_sss@tut.by>
2019-03-17 22:21:35 +0300

    links for pages

--- script/main.js
+++ script/main.js
@@ -1,21 +1,3 @@
-// function showSlide(slideNumber) {
-//     const slides = document.querySelector('.slider__inner').children;
-//     let currentSlideIndex;
-//     Array.prototype.forEach.call(slides, (slide, i) => {
-//         if (!slide.classList.contains('hide')) {
-//             slide.classList.add('hide');
-//             currentSlideIndex = i;
-//         }
-//     });
-//     let nextSlideIndex;
-//     if (currentSlideIndex >= slides.length-1) {
-//         nextSlideIndex = 0;
-//     } else {
-//         nextSlideIndex = currentSlideIndex + 1;
-//     }
-//     slides[nextSlideIndex].classList.remove('hide');
-// }
-// setInterval(showSlide, 2000);
 
         // JQuery
 

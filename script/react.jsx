8e7dcb9544e545e43dbeaea4e7f70f65851ce916
Pavel <Pasha_sss@tut.by>
2019-03-21 00:55:18 +0300

    filter

--- script/react.jsx
+++ script/react.jsx
@@ -75,6 +75,52 @@ const popular = [
 
 ]
 
+const catalog = [
+  {
+    id: 1,
+    title: 'Joytech eGo AIO 1500mAh Black-White',
+    image: 'img/ego_AIO.jpg',
+    price: 123,
+    type: 'sig'
+  },
+  {
+    id: 2,
+    title: 'SMOK Alien 220W AVP kit gun',
+    image: 'img/alien.jpg',
+    price: 2000,
+    type: 'sig'
+  },
+  {
+    id: 3,
+    title: 'Eleaf IJust S 3000mAh',
+    image: 'img/ijust.jpg',
+    price: 1700,
+    type: 'sig',
+  },
+  {
+    id: 4,
+    title: 'Жидкость milk cream green',
+    image: 'img/milk_cream_green.jpg',
+    price: 345,
+    type: 'fl'
+  },
+  {
+    id: 5,
+    title: 'Жидкость mr. Blintz',
+    image: 'img/mr_blintz.jpg',
+    price: 678,
+    type: 'fl'
+  },
+  {
+    id: 6,
+    title: 'Жидкость old school tea fresh',
+    image: 'img/tea_time.jpg',
+    price: 234,
+    type: 'fl'
+  }
+  
+]
+
 ReactDOM.render(
   <App posts={data} />,
   document.querySelector(".products")
@@ -87,10 +133,48 @@ function pop(event){
   document.querySelector(".products")
   );
 }
+
 function getNew(event){
   event.preventDefault();
   ReactDOM.render(
   <App posts={data} />,
   document.querySelector(".products")
+  );
+}
+
+function getCatalog(event){
+  event.preventDefault();
+  ReactDOM.render(
+    <div className="products">
+      <h2 className='layout'>Католог товаров</h2>
+      <App posts={catalog} />
+    </div>,
+    document.querySelector("section")
+  );
+}
+
+function showSigs(event, pur){
+  event.preventDefault();
+  const products = [];
+  catalog.forEach(x => {
+    if(x.type === pur) {
+
+      const product = (
+        <div key={x.id} className="product">
+          <News  title={x.title} image={x.image} price={x.price} />
+        </div>
+      );
+
+      products.push(product);
+    }
+  });
+  ReactDOM.render(
+    <div className="products">
+      <img src="./img/info_3.jpg" />
+      <div>
+        {products}
+      </div>
+    </div>,
+    document.querySelector("section")
   );
 }
